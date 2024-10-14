f61ede0accd0fccac68b9aeda2d3a522c2ca09ec
Aristides Lima <aristides.lima@gmail.com>
2024-10-13 22:42:29 -0300

    Novas atualizações - Rotas 100% construídas

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -21,12 +21,55 @@ class MyTask(db.Model):
     content = db.Column(db.String(100),nullable=False)
     complete = db.Column(db.Integer, default=0)
     created = db.Column(db.DateTime,default=datetime.utcnow)
-    def __repr__(self) -> str:
-        return  f"task{self.id}"
 
-@app.route("/")
+    def __repr__(self) -> str:
+        return  f"task {self.id}"
+
+# Routes do webpages
+# Home Page
+# Rota Inicial - Função de Consulta as Tasks e de Inclusão de Tasks
+@app.route("/", methods=["POST","GET"])
 def index():
-    return render_template("index.html")
+    # Add A Task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR:{e}")
+            return f"ERROR:{e}"
+    # See All Current Tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template('index.html', tasks=tasks)
+
+# Rota para exclusão das Tasks
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"ERROR:{e}"
+
+# Rota para edição das Tasks
+@app.route("/edit/<int:id>", methods=["GET","POST"])
+def edit(id:int):
+    task = MyTask.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"ERROR:{e}"
+    else:
+        return render_template('edit.html', task=task)
 
 if __name__ in "__main__":
     with app.app_context():
